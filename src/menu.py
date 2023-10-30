d24e9bcfc5462961b6da81f97284567bb0accc2c
Neia Finch <neia@ikeran.org>
2023-10-29 17:33:46 -0700

    modes and menus

---
+++ src/menu.py
@@ -0,0 +1,87 @@
+import pyxel
+
+import collections
+
+from dataclasses import dataclass, field
+from typing import Callable, Any
+
+@dataclass
+class Button:
+    title: str
+    action: Callable[[Any], None]
+
+@dataclass
+class Color:
+    bg: int = 0
+    fg: int = 6
+    selected: int = 11
+
+@dataclass
+class Style:
+    color: Color = field(default_factory = Color)
+    border_color: Color = field(default_factory = Color)
+    width: int = 80
+    button_height: int = 10
+    button_width: int = 70
+    margin: int = 2
+    border_size: int = 1
+    selected_border_size: int = 1
+
+class MenuMode:
+    def __init__(self, title, buttons, style = None):
+        self.title = title
+        self.buttons = buttons
+        self.selected = 0
+        self.style = style or Style()
+
+    def update(self, app):
+        if pyxel.btn(pyxel.MOUSE_BUTTON_LEFT):
+            # which button are we hovering over?
+            pass
+        if pyxel.btn(pyxel.KEY_DOWN):
+            self.selected += 1
+            if self.selected >= len(self.buttons):
+                self.selected = len(self.buttons) - 1
+        if pyxel.btn(pyxel.KEY_UP):
+            self.selected -= 1
+            if self.selected < 0:
+                self.selected = 0
+        if pyxel.btn(pyxel.KEY_RETURN):
+            self.buttons[self.selected].action(app)
+
+    def draw(self):
+        pyxel.cls(self.style.color.bg)
+        pyxel.text(self.style.margin, self.style.margin, self.title, self.style.color.fg)
+        top_margin = 10
+        left = (self.style.width - self.style.button_width) // 2
+        button_width = self.style.width - 2 * self.style.margin
+        layout_height = self.style.button_height + self.style.margin
+        text_offset = (self.style.button_height - 5) // 2
+        text_left = left + self.style.margin + self.style.border_size
+        for i, button in enumerate(self.buttons):
+            top = top_margin + i * layout_height
+            border_fg = self.style.border_color.fg
+            text_fg = self.style.color.fg
+            if i == self.selected:
+                border_fg = self.style.border_color.selected
+                text_fg = self.style.color.selected
+            # first the border
+            pyxel.rect(left + 1, top + 1, self.style.button_width, self.style.button_height,
+                    self.style.border_color.bg)
+            pyxel.rect(left, top, self.style.button_width, self.style.button_height,
+                    border_fg)
+            # inside
+            pyxel.rect(
+                    left + 1,
+                    top + 1,
+                    self.style.button_width - 2,
+                    self.style.button_height - 2,
+                    self.style.color.bg)
+            # text
+            pyxel.text(text_left, top + text_offset, button.title, text_fg)
+
+def pause_menu():
+    return MenuMode("Main Menu", [
+        Button("Quit", lambda app: pyxel.quit()),
+        Button("Continue", lambda app: app.pop_mode()),
+    ])
--- src/skyrom.py
+++ src/skyrom.py
@@ -1,5 +1,6 @@
 import pyxel
 import palette
+import menu
 
 from datetime import *
 
@@ -12,19 +13,13 @@ def clamp(min_value, x, max_value):
         return max_value
     return x
 
-class App:
-    def __init__(self):
-        self.color = 0
-        self.tilemap = 0
+class MapMode:
+    def __init__(self, tilemap):
+        self.tilemap = tilemap
         self.charpos = (76, 46)
-        pyxel.init(160, 100, title="Skyrom", fps=10, quit_key=pyxel.KEY_NONE)
-        pyxel.load("../res/game.pyxres")
         self.tm = pyxel.tilemap(0)
-        pyxel.run(self.update, self.draw)
 
-    def update(self):
-        if pyxel.btn(pyxel.KEY_Q):
-            pyxel.quit()
+    def update(self, app):
         if pyxel.btn(pyxel.KEY_DOWN):
             self.charpos = (self.charpos[0], self.charpos[1] + MOVE_SPEED)
         if pyxel.btn(pyxel.KEY_UP):
@@ -33,6 +28,8 @@ class App:
             self.charpos = (self.charpos[0] - MOVE_SPEED, self.charpos[1])
         if pyxel.btn(pyxel.KEY_RIGHT):
             self.charpos = (self.charpos[0] + MOVE_SPEED, self.charpos[1])
+        if pyxel.btn(pyxel.KEY_ESCAPE):
+            app.push_mode(menu.pause_menu())
         self.charpos = (
                 clamp(0, self.charpos[0], self.tm.width - 8),
                 clamp(0, self.charpos[1], self.tm.height - 8))
@@ -43,6 +40,32 @@ class App:
     def draw(self):
         pyxel.bltm(0, 0, self.tilemap, 0, 0, 256, 256)
         pyxel.blt(self.charpos[0], self.charpos[1], 2, 0, 0, 10, 10, 14)
+        pyxel.rect(4, 4, 20, 10, 0)
+        pyxel.text(5, 5, "text!", 5)
+
+class App:
+    def __init__(self):
+        pyxel.init(160, 100, title="Skyrom", fps=10, quit_key=pyxel.KEY_NONE)
+        pyxel.load("../res/game.pyxres")
+        self.mode = MapMode(0)
+        self.mode_stack = [self.mode]
+        pyxel.run(self.update, self.draw)
+
+    def push_mode(self, mode):
+        self.mode_stack.append(mode)
+        self.mode = mode
+
+    def pop_mode(self):
+        if len(self.mode_stack) < 2:
+            raise Exception("no more modes to pop")
+        self.mode_stack = self.mode_stack[:-1]
+        self.mode = self.mode_stack[-1]
+
+    def update(self):
+        self.mode.update(self)
+
+    def draw(self):
+        self.mode.draw()
 
 App()
 
